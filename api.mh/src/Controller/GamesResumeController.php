7028a772c7cd20a70e9623318413a4dbc010a5f9
ImJustLucas <bellierlucas.pro@gmail.com>
2022-09-26 16:28:42 +0200

    ✨ Create Games Resume

---
+++ api.mh/src/Controller/GamesResumeController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+
+class GamesResumeController extends AbstractController
+{
+    #[Route('/games/{username}', name: 'app_games_resume', method: ['GET'])]
+    public function index(string $username): JsonResponse
+    {
+        return $this->json([
+            'message' => 'Get the resume of the last X games',
+            'username' => $username,
+        ]);
+    }
+}
