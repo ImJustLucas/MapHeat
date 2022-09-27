46d04d44c07ac3eb80e07c228c02fc968a8234ae
unknown <CesTheo>
2022-09-27 16:18:02 +0200

    update

---
+++ front.mh/src/component/PlayerResume.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+
+export default class PlayerResume extends Component {
+  render() {
+    return (
+      <div>
+        <div className='m-5 p-2 match bg-slate-300 rounded flex flex-row justify-between'>
+            <div>
+                Type de Partie
+                V/D
+                Time
+            </div>
+            <div>
+                <p>d</p>
+            </div>
+            <div>
+                Case C
+            </div>
+        </div>
+      </div>
+    )
+  }
+}
