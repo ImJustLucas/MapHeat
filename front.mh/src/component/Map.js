e1df7813a8b693d70a6484498705bf299042430e
cestheo <theomaudet03@gmail.com>
2022-09-29 14:56:55 +0200

    map kill show

--- front.mh/src/component/Map.js
+++ front.mh/src/component/Map.js
@@ -1,25 +1,99 @@
-import React, { Component, useState } from 'react';
-// import axios from 'axios';
+import React, { Component, createElement, useEffect, useState } from 'react';
+import { ReactDOM } from 'react';
 import DataHero from './../Data/heroes.json';
+import Game from './../Data/gametest.json';
 
 export default class Map extends Component {
     constructor(props){
         super(props);
 
         this.state = {
-            hero: "TwistedFate",
+            hero: "Lux",
+            timer: 0,
+            frame: 0,
+            start: false,
         };
-
-        console.log(DataHero.data[this.state.hero])
     }
+
+    //-----------------------------------------//
+                    //FUNCTION
+    //-----------------------------------------//
+
+    MovePlayerTo(playerid, x, y){
+        const MovePlayer = document.getElementById(playerid);
+        MovePlayer.style.left = 'calc(' + x.toString() + '% - 15px )';
+        MovePlayer.style.top = 'calc(' + y.toString() + '% - 15px )';
+    }
+    //Pose les events Kill sur la cart
+    SetEventKill(x, y){
+        if(this.state.start == true){
+            const killicon = document.createElement('div')
+            killicon.className = 'Lastkill'
+            killicon.style.left = 'calc(' + this.ConvertCoordToPercentX(x).toString() + '% - 15px )';
+            killicon.style.top= 'calc(' + this.ConvertCoordToPercentY(y).toString() + '% - 15px )';
+            document.getElementById("insert").parentNode.insertBefore(killicon, document.getElementById('insert'))
+        }
+    }
+    //Convertir les Coord LOL en Pourcentage
+    ConvertCoordToPercent(valuex, valuey){
+        valuex = (valuex/15000) * 100;
+        valuey = (valuey/15000) * 100;
+        return {valuex, valuey}
+    }
+
+    ConvertCoordToPercentX(valuex){
+        valuex = (valuex/15000) * 100;
+        return valuex
+    }
+
+    ConvertCoordToPercentY(valuey){
+        valuey = (valuey/15000) * 100;
+        return valuey
+    }
+    //-----------------------------------------//
+                    //DidMount
+    //-----------------------------------------//
+
 
     componentDidMount(){
-    }
-    render() {
+        this.MovePlayerTo("1", 0, 0);
+        this.MovePlayerTo("2", 100, 0);
+        this.SetEventKill("1", "2", 50, 50);
 
+        const frame = Game.info.frames[7].events
+        // console.log(frame[31].position.x)
+
+        const ReadFrame = frame.forEach(element => {
+            this.state.start = true;
+            if(element.type == "CHAMPION_KILL"){
+                return this.SetEventKill(element.position.x, element.position.y)
+            }
+        });
+
+    }
+
+    //-----------------------------------------//
+                    //Render
+    //-----------------------------------------//
+    
+    render() {
+        
         const backgroundImageLol = {
             backgroundImage: 'url(https://ddragon.leagueoflegends.com/cdn/img/champion/splash/'+ this.state.hero +'_0.jpg)'
         }
+        
+        const Player1 = {
+            backgroundImage: 'url(https://ddragon.leagueoflegends.com/cdn/12.18.1/img/champion/' + this.state.hero + '.png)',
+            display: "none"
+        }
+
+        const Player2 = {
+            backgroundImage: 'url(https://ddragon.leagueoflegends.com/cdn/12.18.1/img/champion/Morgana.png)',
+            display: "none"
+        }
+        
+
+
     
         return (
             <div className='map-size'>
@@ -30,16 +104,21 @@ export default class Map extends Component {
                 </div>
                 <div className='my-3'>
                     <h1 className='text-white text-2xl text-center'>Analysons votre partie du 27/09/2022 à 19h54</h1>
+                    <h1 className='text-white text-2xl text-center'>{this.state.timer}</h1>
                     <div className='flex flex-row justify-center content-end w-full'>
                         <h2 className='text-white text-xl text-center'>Vous avez joué le champion {this.state.hero}</h2>
                         <img src={`https://ddragon.leagueoflegends.com/cdn/12.18.1/img/champion/${this.state.hero}.png`} width="50px" className='ml-2'></img>
                     </div>
-                    {/* <h2 className='text-white text-xl text-center' dangerouslySetInnerHTML={{__html: DataHero.data[this.state.hero].blurb}}></h2> */}
-                    <h2 className='text-white text-xl text-center'>{DataHero.data[this.state.hero].title}</h2>
-                    {/* <img src={`https://ddragon.leagueoflegends.com/cdn/12.18.1/img/champion/${this.state.hero}.png`}></img> */}
                 </div>
-                <div className='map'>
-
+                <div className='map' id='map'>
+                    <div className='player' style={Player1} id="1"></div>
+                    <div className='player' style={Player2} id="2"></div>
+                    <div id="insert">
+                    </div>
+                </div>
+                <div class="slidecontainer">
+                    <p className='text-white text-3xl'>Frame : </p>
+                    <input type="range" min="0" max="10" value="0" step="1" id="Frame" />
                 </div>
             </div>
         )
